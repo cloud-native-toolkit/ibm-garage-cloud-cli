d0ad7fdeb7a7858792e4290ec89f5bc983f864a4
Sean Sundberg <seansund@us.ibm.com>
2019-06-13 15:04:37 -0500

    Puts deploy-image.sh back to the way it was

--- bin/deploy-image.sh
+++ bin/deploy-image.sh
@@ -21,7 +21,7 @@ if [[ -z "${REGION}" ]]; then
   exit 1
 fi
 
-if [[ "${REGISTRY_URL}" =~ .*icr.io ]] && [[ -z "${REGISTRY_NAMESPACE}" ]]; then
+if [[ -z "${REGISTRY_NAMESPACE}" ]]; then
   echo "REGISTRY_NAMESPACE is required"
   exit 1
 fi
@@ -31,38 +31,41 @@ if [[ -z "${CLUSTER_NAME}" ]]; then
   exit 1
 fi
 
+if [[ -z "${REGISTRY_URL}" ]]; then
+  echo "REGISTRY_URL is required"
+  exit 1
+fi
+
 if [[ -z "${TMP_DIR}" ]]; then
   TMP_DIR="/tmp"
 fi
 
-CLUSTER_NAMESPACE="$1"
+IMAGE_NAME="$1"
+CHART_NAME="$1"
+IMAGE_VER="$2"
+ENVIRONMENT_NAME="$3"
 
-IMAGE_NAME="$2"
-if [[ -z "${CHART_NAME}" ]]; then
-  CHART_NAME="${IMAGE_NAME}"
-fi
-
-IMAGE_VER="$3"
-
-if [[ -z "${CLUSTER_NAMESPACE}" ]] || [[ "${CLUSTER_NAMESPACE}" = "undefined" ]]; then
-  echo "Cluster namespace required as first arg"
+if [[ -z "${IMAGE_NAME}" ]] || [[ "${IMAGE_NAME}" = "undefined" ]]; then
+  echo "Image name required as first arg"
   exit 1
 fi
 
-if [[ -z "${VALUES_FILE}" ]]; then
-    if [[ -z "${IMAGE_NAME}" ]] || [[ "${IMAGE_NAME}" = "undefined" ]]; then
-      echo "Image name required as second arg"
-      exit 1
-    fi
+if [[ -z "${IMAGE_VER}" ]] || [[ "${IMAGE_VER}" = "undefined" ]]; then
+  echo "Image version required as second arg"
+  exit 1
+fi
 
-    if [[ -z "${IMAGE_VER}" ]] || [[ "${IMAGE_VER}" = "undefined" ]]; then
-      echo "Image version required as third arg"
-      exit 1
-    fi
+if [[ -z "${ENVIRONMENT_NAME}" ]] || [[ "${ENVIRONMENT_NAME}" = "undefined" ]]; then
+  echo "Environment name required as third arg"
+  exit 1
 fi
 
 if [[ -z "${CHART_ROOT}" ]]; then
   CHART_ROOT="."
+fi
+
+if [[ -z "${CLUSTER_NAMESPACE}" ]]; then
+  CLUSTER_NAMESPACE="${CLUSTER_NAME}-${ENVIRONMENT_NAME}"
 fi
 
 CHART_PATH="${CHART_ROOT}/${CHART_NAME}"
@@ -92,21 +95,18 @@ else
 fi
 echo "RELEASE_NAME: $RELEASE_NAME"
 
+ibmcloud cr images --restrict ${REGISTRY_NAMESPACE}/${IMAGE_NAME} -q > ${TMP_DIR}/.images
+if [[ -n "${IMAGE_BUILD_NUMBER}" ]]; then
+  grep "${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}-${IMAGE_BUILD_NUMBER}" ${TMP_DIR}/.images -q
+  if [[ $? -eq 0 ]]; then
+    IMAGE_VER="${IMAGE_VER}-${IMAGE_BUILD_NUMBER}"
+  fi
+fi
 
-if [[ "${REGISTRY_URL}" =~ .*icr.io ]]; then
-    ibmcloud cr images --restrict ${REGISTRY_NAMESPACE}/${IMAGE_NAME} -q > ${TMP_DIR}/.images
-    if [[ -n "${IMAGE_BUILD_NUMBER}" ]]; then
-      grep "${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}-${IMAGE_BUILD_NUMBER}" ${TMP_DIR}/.images -q
-      if [[ $? -eq 0 ]]; then
-        IMAGE_VER="${IMAGE_VER}-${IMAGE_BUILD_NUMBER}"
-      fi
-    fi
-
-    grep "${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}" ${TMP_DIR}/.images -q
-    if [[ $? -ne 0 ]]; then
-      echo "Image not found: ${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}"
-      exit 1
-    fi
+grep "${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}" ${TMP_DIR}/.images -q
+if [[ $? -ne 0 ]]; then
+  echo "Image not found: ${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}"
+  exit 1
 fi
 
 ibmcloud ks cluster-pull-secret-apply --cluster ${CLUSTER_NAME}
@@ -123,32 +123,15 @@ helm init --upgrade
 echo "CHECKING CHART (lint)"
 helm lint ${CHART_PATH}
 
-IMAGE_REPOSITORY="${IMAGE_NAME}"
-if [[ -n "${REGISTRY_NAMESPACE}" ]]; then
-  IMAGE_REPOSITORY="${REGISTRY_NAMESPACE}/${IMAGE_REPOSITORY}"
-fi
-
-if [[ -n "${REGISTRY_URL}" ]]; then
-  IMAGE_REPOSITORY="${REGISTRY_URL}/${IMAGE_REPOSITORY}"
-fi
-
-PIPELINE_IMAGE_URL="${IMAGE_REPOSITORY}:${IMAGE_VER}"
-
-if [[ -n "${VALUES_FILE}" ]]; then
-    echo -e "Dry run into: ${CLUSTER_NAME}/${CLUSTER_NAMESPACE}."
-    helm upgrade --install --debug --dry-run ${RELEASE_NAME} ${CHART_PATH} --namespace ${CLUSTER_NAMESPACE} --values ${VALUES_FILE}
-
-    echo -e "Deploying into: ${CLUSTER_NAME}/${CLUSTER_NAMESPACE}."
-    helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} --namespace ${CLUSTER_NAMESPACE} --values ${VALUES_FILE}
-else
-    echo -e "Dry run into: ${CLUSTER_NAME}/${CLUSTER_NAMESPACE}."
-    helm upgrade --install --debug --dry-run ${RELEASE_NAME} ${CHART_PATH} --set image.repository=${IMAGE_REPOSITORY},image.tag=${IMAGE_VER},image.secretName="${CLUSTER_NAMESPACE}-us-icr-io" --namespace ${CLUSTER_NAMESPACE}
-
-    echo -e "Deploying into: ${CLUSTER_NAME}/${CLUSTER_NAMESPACE}."
-    helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} --set image.repository=${IMAGE_REPOSITORY},image.tag=${IMAGE_VER},image.secretName="${CLUSTER_NAMESPACE}-us-icr-io" --namespace ${CLUSTER_NAMESPACE}
-fi
+IMAGE_REPOSITORY="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}"
+PIPELINE_IMAGE_URL="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_VER}"
 
 # Using 'upgrade --install" for rolling updates. Note that subsequent updates will occur in the same namespace the release is currently deployed in, ignoring the explicit--namespace argument".
+echo -e "Dry run into: ${CLUSTER_NAME}/${CLUSTER_NAMESPACE}."
+helm upgrade --install --debug --dry-run ${RELEASE_NAME} ${CHART_PATH} --set image.repository=${IMAGE_REPOSITORY},image.tag=${IMAGE_VER},image.secretName="${CLUSTER_NAMESPACE}-us-icr-io" --namespace ${CLUSTER_NAMESPACE}
+
+echo -e "Deploying into: ${CLUSTER_NAME}/${CLUSTER_NAMESPACE}."
+helm upgrade --install ${RELEASE_NAME} ${CHART_PATH} --set image.repository=${IMAGE_REPOSITORY},image.tag=${IMAGE_VER},image.secretName="${CLUSTER_NAMESPACE}-us-icr-io" --namespace ${CLUSTER_NAMESPACE}
 
 ${SCRIPT_ROOT}/deploy-checkstatus.sh ${CLUSTER_NAMESPACE} ${IMAGE_NAME} ${IMAGE_REPOSITORY} ${IMAGE_VER}
 
