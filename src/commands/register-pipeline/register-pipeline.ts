f88c74e6088ea8a3d0019d9f2b0c0abd74c7e6c1
Sean Sundberg <seansund@us.ibm.com>
2019-06-16 00:13:32 -0500

    Adds logic to check of KUBECONFIG in register-pipeline.ts before shell is called

--- src/commands/register-pipeline/register-pipeline.ts
+++ src/commands/register-pipeline/register-pipeline.ts
@@ -1,62 +1,88 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import {execFile} from 'child_process';
+import * as readline from 'readline';
+import * as opn from 'opn';
 
 import {RegisterPipelineOptions} from './register-pipeline-options.model';
 import {deployImage, DeployOptions} from '../deploy-image';
-import {execFile} from 'child_process';
 
 export async function registerPipeline(options: RegisterPipelineOptions) {
 
-    const valuesFile = path.join(process.cwd(), '.tmp/register-pipeline-values.yaml');
-    const releaseName = process.cwd().replace(/.*\/(.*)/, "$1");
-
-    await new Promise((resolve, reject) => {
-        const child = execFile(
-          path.join(__dirname, '../../../bin/generate-git-values.sh'),
-          [valuesFile],
-          {
-              cwd: process.cwd()
-          },
-          (error, stdout, stderr) => {
-              if (error) {
-                  reject(error);
-              }
-
-              resolve({stdout, stderr});
-          }
-        );
-
-        child.stdout.pipe(process.stdout);
-        child.stderr.pipe(process.stderr);
-        process.stdin.pipe(child.stdin);
+  if (!process.env.KUBECONFIG) {
+    console.log("KUBECONFIG environment variable not found. It appears the kubernetes environment has not been initialized.")
+    console.log("To initialize the kubernetes:");
+    console.log(" 1) Navigate to https://cloud.ibm.com/kubernetes/clusters");
+    console.log(" 2) Select the kubernetes cluster");
+    console.log(" 3) Follow the instructions on the access tab");
+    console.log("");
+    console.log("Open the URL in the default browser? [Y/n]> ");
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
     });
 
-    await new Promise((resolve, reject) => {
-        const child = execFile(
-          path.join(__dirname, '../../../bin/register-pipeline.sh'),
-          [options.namespace, releaseName, valuesFile],
-          {
-              cwd: process.cwd(),
-              env: Object.assign(
-                {},
-                process.env,
-                {
-                  CLUSTER_NAMESPACE: options.namespace
-                },
-              ),
-          },
-          (error, stdout, stderr) => {
-              if (error) {
-                  reject(error);
-              }
+    rl.on('line', function(line){
+      if (line === 'n') {
+        process.exit(1);
+      }
 
-              resolve({stdout, stderr});
-          },
-        );
-
-        child.stdout.pipe(process.stdout);
-        child.stderr.pipe(process.stderr);
+      opn('https://cloud.ibm.com/kubernetes/clusters');
+      process.exit(1);
     });
+  }
 
-    await fs.promises.unlink(valuesFile);
+  const valuesFile = path.join(process.cwd(), '.tmp/register-pipeline-values.yaml');
+  const releaseName = process.cwd().replace(/.*\/(.*)/, '$1');
+
+  await new Promise((resolve, reject) => {
+    const child = execFile(
+      path.join(__dirname, '../../../bin/generate-git-values.sh'),
+      [valuesFile],
+      {
+        cwd: process.cwd()
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        }
+
+        resolve({stdout, stderr});
+      }
+    );
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    process.stdin.pipe(child.stdin);
+  });
+
+  await new Promise((resolve, reject) => {
+    const child = execFile(
+      path.join(__dirname, '../../../bin/register-pipeline.sh'),
+      [options.namespace, releaseName, valuesFile],
+      {
+        cwd: process.cwd(),
+        env: Object.assign(
+          {},
+          process.env,
+          {
+            CLUSTER_NAMESPACE: options.namespace
+          },
+        ),
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        }
+
+        resolve({stdout, stderr});
+      },
+    );
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+  });
+
+  await fs.promises.unlink(valuesFile);
 }
