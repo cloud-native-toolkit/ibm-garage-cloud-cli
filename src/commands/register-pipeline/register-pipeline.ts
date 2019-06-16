976062f61115cf8d35e351764e623e24596902c1
Sean Sundberg <seansund@us.ibm.com>
2019-06-16 00:23:42 -0500

    Adds logic to check of KUBECONFIG in register-pipeline.ts before shell is called

--- src/commands/register-pipeline/register-pipeline.ts
+++ src/commands/register-pipeline/register-pipeline.ts
@@ -9,29 +9,7 @@ import {deployImage, DeployOptions} from '../deploy-image';
 
 export async function registerPipeline(options: RegisterPipelineOptions) {
 
-  if (!process.env.KUBECONFIG) {
-    console.log("KUBECONFIG environment variable not found. It appears the kubernetes environment has not been initialized.")
-    console.log("To initialize the kubernetes:");
-    console.log(" 1) Navigate to https://cloud.ibm.com/kubernetes/clusters");
-    console.log(" 2) Select the kubernetes cluster");
-    console.log(" 3) Follow the instructions on the access tab");
-    console.log("");
-    console.log("Open the URL in the default browser? [Y/n]> ");
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      terminal: false
-    });
-
-    rl.on('line', function(line){
-      if (line === 'n') {
-        process.exit(1);
-      }
-
-      opn('https://cloud.ibm.com/kubernetes/clusters');
-      process.exit(1);
-    });
-  }
+  await checkKubeconfig();
 
   const valuesFile = path.join(process.cwd(), '.tmp/register-pipeline-values.yaml');
   const releaseName = process.cwd().replace(/.*\/(.*)/, '$1');
@@ -85,4 +63,35 @@ export async function registerPipeline(options: RegisterPipelineOptions) {
   });
 
   await fs.promises.unlink(valuesFile);
+}
+
+async function checkKubeconfig() {
+  return new Promise((resolve, reject) => {
+    if (!process.env.KUBECONFIG) {
+      console.log('KUBECONFIG environment variable not found. It appears the kubernetes environment has not been initialized.');
+      console.log('To initialize the kubernetes:');
+      console.log(' 1) Navigate to https://cloud.ibm.com/kubernetes/clusters');
+      console.log(' 2) Select the kubernetes cluster');
+      console.log(' 3) Follow the instructions on the access tab');
+      console.log('');
+      process.stdout.write('Open the URL in the default browser? [Y/n]> ');
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+      });
+
+      rl.on('line', function (line) {
+        if (line === 'n') {
+          reject(new Error('KUBECONFIG not set'));
+        }
+
+        opn('https://cloud.ibm.com/kubernetes/clusters');
+        reject(new Error('KUBECONFIG not set'));
+      });
+    } else {
+      resolve();
+    }
+  });
 }
