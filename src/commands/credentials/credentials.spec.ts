8796d3e6c71afafd0d248bc231515891d27ed59d
Sean Sundberg <seansund@us.ibm.com>
2019-08-15 00:16:47 -0500

    v0.0.98 - Updates credentials command to include artifactory values and refactors logic to make it easier to add others in the future

--- src/commands/credentials/credentials.spec.ts
+++ src/commands/credentials/credentials.spec.ts
@@ -1,15 +1,13 @@
 import rewire = require('rewire');
-import {buildMockKubeClient} from '../../api/kubectl/testHelper';
+import {Secrets} from './credentials';
 
 const module = rewire('./credentials');
 
 const getCredentials = module.__get__('getCredentials');
-const getJenkinsInfo = module.__get__('getJenkinsInfo');
-const getSonarqubeInfo = module.__get__('getSonarqubeInfo');
-const getSonarqubeCredentials = module.__get__('getSonarqubeCredentials');
-const getSonarqubeUrl = module.__get__('getSonarqubeUrl');
-const getArgoCdInfo = module.__get__('getArgoCdInfo');
-const getArgoCdUrl = module.__get__('getArgoCdUrl');
+const configMaps = module.__get__('configMaps');
+const secrets = module.__get__('secrets');
+const flatten = module.__get__('flatten');
+const group = module.__get__('group');
 
 describe('credentials', () => {
   test('canary verifies test infrastructure', () => {
@@ -18,297 +16,266 @@ describe('credentials', () => {
 
   describe('given getCredentials()', () => {
 
-    let mock_getJenkinsInfo;
-    let unset_getJenkinsInfo;
-
-    let mock_getArgoCdInfo;
-    let unset_getArgoCdInfo;
-
-    let mock_getSonarqubeInfo;
-    let unset_getSonarqubeInfo;
-
-    beforeEach(() => {
-      mock_getJenkinsInfo = jest.fn();
-      unset_getJenkinsInfo = module.__set__('getJenkinsInfo', mock_getJenkinsInfo);
-
-      mock_getArgoCdInfo = jest.fn();
-      unset_getArgoCdInfo = module.__set__('getArgoCdInfo', mock_getArgoCdInfo);
-
-      mock_getSonarqubeInfo = jest.fn();
-      unset_getSonarqubeInfo = module.__set__('getSonarqubeInfo', mock_getSonarqubeInfo);
-    });
-
-    afterEach(() => {
-      unset_getJenkinsInfo();
-      unset_getArgoCdInfo();
-      unset_getSonarqubeInfo();
-    });
-
-    describe('when called', () => {
-      const jenkins = {username: 'jenkins'};
-      const argocd = {username: 'argocd'};
-      const sonarqube = {username: 'sonarqube'};
-
-      beforeEach(() => {
-        mock_getJenkinsInfo.mockResolvedValue(jenkins);
-        mock_getArgoCdInfo.mockResolvedValue(argocd);
-        mock_getSonarqubeInfo.mockResolvedValue(sonarqube);
-      });
-
-      test('returns values from jenkins, argocd, and sonarqube', async () => {
-        expect(await getCredentials()).toEqual({jenkins, argocd, sonarqube});
-      });
-    });
-  });
-
-  describe('given getJenkinsInfo()', () => {
     let mock_getSecretData;
     let unset_getSecretData;
 
-    beforeEach(() => {
-      mock_getSecretData = jest.fn();
-      unset_getSecretData = module.__set__('getSecretData', mock_getSecretData);
-    });
+    let mock_configMaps;
+    let unset_configMaps;
 
-    afterEach(() => {
-      unset_getSecretData();
-    });
-
-    describe('when jenkins-access secret exists', () => {
-      const url = 'url';
-      const username = 'username';
-      const password = 'password';
-      const api_token = 'api_token';
-
-      beforeEach(() => {
-        mock_getSecretData.mockReturnValue({
-          url,
-          username,
-          password,
-          api_token
-        });
-      });
-
-      test('return {url, username, password, api_token} from secret', async () => {
-        const result = await getJenkinsInfo();
-
-        expect(result).toEqual({url, username, password, api_token});
-      });
-    });
-
-    describe('when no jenkins secrets exist', () => {
-      const username = 'username';
-      const password = 'password';
-
-      beforeEach(() => {
-        mock_getSecretData.mockRejectedValue(new Error('no secret found'));
-      });
-
-      test('return empty object', async () => {
-        const result = await getJenkinsInfo();
-
-        expect(result).toEqual({});
-      });
-    });
-  });
-
-  describe('given getSonarqubeInfo()', () => {
-    let mock_getSonarqubeCredentials;
-    let unset_getSonarqubeCredentials;
-
-    let mock_getSonarqubeUrl;
-    let unset_getSonarqubeUrl;
-
-    beforeEach(() => {
-      mock_getSonarqubeCredentials = jest.fn();
-      unset_getSonarqubeCredentials = module.__set__('getSonarqubeCredentials', mock_getSonarqubeCredentials) as () => void;
-
-      mock_getSonarqubeUrl = jest.fn();
-      unset_getSonarqubeUrl = module.__set__('getSonarqubeUrl', mock_getSonarqubeUrl) as () => void;
-    });
-
-    afterEach(() => {
-      unset_getSonarqubeCredentials();
-      unset_getSonarqubeUrl();
-    });
-
-    describe('when called', () => {
-      const credentials = {username: 'username'};
-      const url = {url: 'url'};
-
-      beforeEach(() => {
-        mock_getSonarqubeCredentials.mockResolvedValue(credentials);
-        mock_getSonarqubeUrl.mockResolvedValue(url);
-      });
-
-      test('return results from sonarqube credentials and url', async () => {
-        expect(await getSonarqubeInfo()).toEqual(Object.assign(credentials, url));
-      });
-    });
-  });
-
-  describe('given getSonarqubeCredentials()', () => {
-    let mock_getSecretData;
-    let unset_getSecretData;
-
-    beforeEach(() => {
-      mock_getSecretData = jest.fn();
-      unset_getSecretData = module.__set__('getSecretData', mock_getSecretData);
-    });
-
-    afterEach(() => {
-      unset_getSecretData();
-    });
-
-    describe('when sonarqube-access secret exists', () => {
-      const username = 'username';
-      const password = 'password';
-
-      beforeEach(() => {
-        mock_getSecretData.mockResolvedValue({
-          username,
-          password,
-        });
-      });
-
-      test('return {username, password} from secret', async () => {
-        const result = await getSonarqubeCredentials();
-
-        expect(result.username).toEqual(username);
-        expect(result.password).toEqual(password);
-      });
-    });
-
-    describe('when no sonarqube-access secret exists', () => {
-      const username = 'username';
-      const password = 'password';
-
-      beforeEach(() => {
-        mock_getSecretData.mockRejectedValue(new Error('no secret found'));
-      });
-
-      test('return empty object', async () => {
-        const result = await getSonarqubeCredentials();
-
-        expect(result).toEqual({});
-      });
-    });
-
-  });
-
-  describe('given getSonarqubeUrl()', () => {
-
-    let mock_getIngressHosts;
-    let unset_getIngressHosts;
-
-    beforeEach(() => {
-      mock_getIngressHosts = jest.fn();
-      unset_getIngressHosts = module.__set__('getIngressHosts', mock_getIngressHosts);
-    });
-
-    afterEach(() => {
-      unset_getIngressHosts();
-    });
-
-    describe('when successfully finds ingress host', () => {
-      const host = 'host';
-
-      beforeEach(() => {
-        mock_getIngressHosts.mockResolvedValue([host]);
-      });
-
-      test('return url', async () => {
-        expect(await getSonarqubeUrl()).toEqual({url: `http://${host}`});
-
-        expect(mock_getIngressHosts.mock.calls[0]).toEqual(['tools', 'sonarqube-sonarqube']);
-      });
-    });
-
-    describe('when fails to find ingress host', () => {
-
-      beforeEach(() => {
-        mock_getIngressHosts.mockRejectedValue(new Error('host not found'));
-      });
-
-      test('return {}', async () => {
-        expect(await getSonarqubeUrl()).toEqual({});
-      });
-    });
-  });
-
-  describe('given getArgoCdInfo()', () => {
     let mock_getArgoCdCredentials;
     let unset_getArgoCdCredentials;
 
-    let mock_getArgoCdUrl;
-    let unset_getArgoCdUrl;
+    let mock_flatten;
+    let unset_flatten;
+
+    let mock_group;
+    let unset_group;
+
+    let mock_secrets;
+    let unset_secrets;
 
     beforeEach(() => {
+      mock_getSecretData = jest.fn();
+      unset_getSecretData = module.__set__('getSecretData', mock_getSecretData) as () => void;
+
+      mock_configMaps = jest.fn();
+      unset_configMaps = module.__set__('configMaps', mock_configMaps) as () => void;
+
+      mock_secrets = jest.fn();
+      unset_secrets = module.__set__('secrets', mock_secrets) as () => void;
+
       mock_getArgoCdCredentials = jest.fn();
       unset_getArgoCdCredentials = module.__set__('getArgoCdCredentials', mock_getArgoCdCredentials) as () => void;
 
-      mock_getArgoCdUrl = jest.fn();
-      unset_getArgoCdUrl = module.__set__('getArgoCdUrl', mock_getArgoCdUrl) as () => void;
+      mock_flatten = jest.fn();
+      unset_flatten = module.__set__('flatten', mock_flatten) as () => void;
+
+      mock_group = jest.fn();
+      unset_group = module.__set__('group', mock_group) as () => void;
     });
 
     afterEach(() => {
+      unset_secrets();
+      unset_configMaps();
       unset_getArgoCdCredentials();
-      unset_getArgoCdUrl();
+      unset_flatten();
+      unset_group();
     });
 
     describe('when called', () => {
-      const credentials = {username: 'username'};
-      const url = {url: 'url'};
+      const credentialCollection = [{}, {}, {}, {}, {}, {}, {}, {}];
+      const credentials = {JENKINS_URL: 'https://jenkins.cloud'};
+      const secrets: Secrets = {jenkins: {}};
 
       beforeEach(() => {
-        mock_getArgoCdCredentials.mockResolvedValue(credentials);
-        mock_getArgoCdUrl.mockResolvedValue(url);
+        mock_getSecretData.mockResolvedValue({});
+        mock_secrets.mockReturnValue([Promise.resolve({}), Promise.resolve({})]);
+        mock_configMaps.mockReturnValue([Promise.resolve({}), Promise.resolve({}), Promise.resolve({}), Promise.resolve({})]);
+        mock_getArgoCdCredentials.mockResolvedValue({});
+
+        mock_flatten.mockReturnValue(credentials);
+        mock_group.mockReturnValue(secrets);
       });
 
-      test('return results from argo-cd credentials and url', async () => {
-        expect(await getArgoCdInfo()).toEqual(Object.assign(credentials, url));
+      test('should call configMaps() for jenkins-config, sonarqube-config, artifactory-config, argocd-config', async () => {
+
+        const namespace = 'namespace';
+        await getCredentials(namespace);
+
+        expect(mock_configMaps).toHaveBeenCalledWith(['jenkins-config', 'sonarqube-config', 'artifactory-config', 'argocd-config'], namespace);
+      });
+
+      test('should call getSecretData() for jenkins-access, sonarqube-access, artifactory-access', async () => {
+
+        const namespace = 'namespace';
+        await getCredentials(namespace);
+
+        expect(mock_secrets).toHaveBeenCalledWith(['sonarqube-access', 'artifactory-access'], namespace);
+      });
+
+      test('should call getArgoCdCredentials()', async () => {
+
+        const namespace = 'namespace';
+        await getCredentials(namespace);
+
+        expect(mock_getArgoCdCredentials).toHaveBeenCalledWith(namespace);
+      });
+
+      test('should pass results from ConfigMap, Secret, and ArgoCdCredentials to flatten()', async () => {
+
+        const namespace = 'namespace';
+        await getCredentials(namespace);
+
+        expect(mock_flatten).toHaveBeenCalledWith(credentialCollection);
+      });
+
+      test('should pass flattened credentials into group()', async () => {
+
+        const namespace = 'namespace';
+        const result = await getCredentials(namespace);
+
+        expect(result).toBe(secrets);
+        expect(mock_group).toHaveBeenCalledWith(credentials);
       });
     });
   });
 
-  describe('given getArgoCdUrl()', () => {
+  describe('given configMaps()', () => {
 
-    let mock_getIngressHosts;
-    let unset_getIngressHosts;
+    let mock_getConfigMapData;
+    let unset_getConfigMapData;
 
     beforeEach(() => {
-      mock_getIngressHosts = jest.fn();
-      unset_getIngressHosts = module.__set__('getIngressHosts', mock_getIngressHosts);
+      mock_getConfigMapData = jest.fn();
+      unset_getConfigMapData = module.__set__('getConfigMapData', mock_getConfigMapData) as () => void;
     });
 
     afterEach(() => {
-      unset_getIngressHosts();
+      unset_getConfigMapData();
     });
 
-    describe('when successfully finds ingress host', () => {
-      const host = 'host';
-      const namespace = 'ns';
+    describe('when getConfigMapData() resolves successfully', () => {
+      const expectedResult = {JENKINS_URL: 'url'};
 
       beforeEach(() => {
-        mock_getIngressHosts.mockResolvedValue([host]);
+        mock_getConfigMapData.mockResolvedValue(expectedResult);
       });
 
-      test('return url', async () => {
-        expect(await getArgoCdUrl(namespace)).toEqual({url: `http://${host}`});
+      test('should call getConfigMapData() for each value in array', async () => {
+        const map1 = 'a';
+        const map2 = 'b';
 
-        expect(mock_getIngressHosts.mock.calls[0]).toEqual([namespace, 'argocd-server-http']);
+        const namespace = 'namespace';
+        configMaps([map1, map2], namespace);
+
+        expect(mock_getConfigMapData).toHaveBeenCalledTimes(2);
+        expect(mock_getConfigMapData.mock.calls[0][0]).toEqual(map1);
+        expect(mock_getConfigMapData.mock.calls[0][1]).toEqual(namespace);
+        expect(mock_getConfigMapData.mock.calls[1][0]).toEqual(map2);
+        expect(mock_getConfigMapData.mock.calls[1][1]).toEqual(namespace);
+      });
+
+      test('should return an array of resolved values', async () => {
+        const namespace = 'namespace';
+        const result = await Promise.all(configMaps(['a', 'b'], namespace));
+
+        expect(result).toEqual([expectedResult, expectedResult]);
       });
     });
 
-    describe('when fails to find ingress host', () => {
+    describe('when getConfigMapData() is rejected', () => {
+      beforeEach(() => {
+        mock_getConfigMapData.mockReturnValue(Promise.reject(new Error('error')));
+      });
+
+      test('should return an array of resolved values', async () => {
+        const namespace = 'namespace';
+        const result = await Promise.all(configMaps(['a', 'b'], namespace));
+
+        expect(result).toEqual([{}, {}]);
+      });
+    });
+  });
+
+  describe('given secrets()', () => {
+
+    let mock_getSecretData;
+    let unset_getSecretData;
+
+    beforeEach(() => {
+      mock_getSecretData = jest.fn();
+      unset_getSecretData = module.__set__('getSecretData', mock_getSecretData) as () => void;
+    });
+
+    afterEach(() => {
+      unset_getSecretData();
+    });
+
+    describe('when getSecretData() resolves successfully', () => {
+      const expectedResult = {JENKINS_URL: 'url'};
 
       beforeEach(() => {
-        mock_getIngressHosts.mockRejectedValue(new Error('host not found'));
+        mock_getSecretData.mockResolvedValue(expectedResult);
       });
 
-      test('return {}', async () => {
-        expect(await getArgoCdUrl()).toEqual({});
+      test('should call getSecretData() for each value in array', async () => {
+        const map1 = 'a';
+        const map2 = 'b';
+
+        const namespace = 'namespace';
+        secrets([map1, map2], namespace);
+
+        expect(mock_getSecretData).toHaveBeenCalledTimes(2);
+        expect(mock_getSecretData.mock.calls[0][0]).toEqual(map1);
+        expect(mock_getSecretData.mock.calls[0][1]).toEqual(namespace);
+        expect(mock_getSecretData.mock.calls[1][0]).toEqual(map2);
+        expect(mock_getSecretData.mock.calls[1][1]).toEqual(namespace);
+      });
+
+      test('should return an array of resolved values', async () => {
+        const namespace = 'namespace';
+        const result = await Promise.all(secrets(['a', 'b'], namespace));
+
+        expect(result).toEqual([expectedResult, expectedResult]);
       });
     });
+
+    describe('when getSecretData() is rejected', () => {
+      beforeEach(() => {
+        mock_getSecretData.mockReturnValue(Promise.reject(new Error('error')));
+      });
+
+      test('should return an array of resolved values', async () => {
+        const namespace = 'namespace';
+        const result = await Promise.all(secrets(['a', 'b'], namespace));
+
+        expect(result).toEqual([{}, {}]);
+      });
+    });
+  });
+
+  describe('given flatten()', () => {
+
+    describe('when called with undefined', () => {
+      test('return an empty object', () => {
+        expect(flatten(undefined)).toEqual({});
+      });
+    });
+
+    describe('when called with an empty array', () => {
+      test('return an empty object', () => {
+        expect(flatten([])).toEqual({});
+      });
+    });
+
+    describe('when called with an array of object', () => {
+      test('return a flattened object', () => {
+        const JENKINS_URL = 'JENKINS_URL';
+        const JENKINS_USER = 'JENKINS_USER';
+        const SONARQUBE_URL = 'SONARQUBE_URL';
+
+        expect(flatten([{JENKINS_URL}, {JENKINS_USER}, {SONARQUBE_URL}])).toEqual({JENKINS_URL, JENKINS_USER, SONARQUBE_URL})
+      });
+    });
+  });
+
+  describe('given group()', () => {
+    describe('when called with empty object', () => {
+      test('return empty object', () => {
+        expect(group({})).toEqual({});
+      });
+    });
+
+    describe('when called with keys having format <group>_<type>', () => {
+      test('return <group>: {<type>: <value>} as lowercase', () => {
+        expect(group({JENKINS_URL: 'url', JENKINS_USER: 'user'})).toEqual({jenkins: {url: 'url', user: 'user'}});
+      });
+    });
+
+    describe('when called with keys not containing underscore', () => {
+      test('ignore the key', () => {
+        expect(group({JENKINSURL: 'url', JENKINS_USER: 'user'})).toEqual({jenkins: {user: 'user'}});
+      });
+    })
   });
 });
--- src/commands/credentials/credentials.ts
+++ src/commands/credentials/credentials.ts
@@ -1,14 +1,15 @@
-import * as secrets from '../../api/kubectl/secrets';
+import * as secretUtil from '../../api/kubectl/secrets';
 import {JenkinsAccessSecret} from '../../model/jenkins-access-secret.model';
 import * as kubeClient from '../../api/kubectl/client';
 import * as ingress from '../../api/kubectl/ingress';
+import * as configMapUtil from '../../api/kubectl/config-map';
 
-let getSecretData = secrets.getSecretData;
-let getIngressHosts = ingress.getIngressHosts;
-let getAllIngressHosts = ingress.getAllIngressHosts;
+let getSecretData = secretUtil.getSecretData;
+let getConfigMapData = configMapUtil.getConfigMapData;
 let buildKubeClient = kubeClient.buildKubeClient;
 
 export interface ComponentCredentials {
+  user?: string;
   username?: string;
   password?: string;
 }
@@ -17,100 +18,137 @@ export interface ComponentUrl {
   url?: string;
 }
 
-export interface ComponentInfo extends ComponentCredentials, ComponentUrl {
-}
+export type ComponentInfo = ComponentCredentials & ComponentUrl
 
 export interface Secrets {
-  jenkins: JenkinsAccessSecret | {};
-  argocd: ComponentInfo;
-  sonarqube: ComponentInfo;
+  jenkins?: ComponentInfo;
+  argocd?: ComponentInfo;
+  sonarqube?: ComponentInfo;
+  artifactory?: ComponentInfo;
 }
+
+interface JenkinsConfig {
+  JENKINS_URL?: string;
+}
+interface JenkinsCredentials {
+  JENKINS_USER?: string;
+  JENKINS_PASSWORD?: string;
+}
+interface SonarqubeConfig {
+  SONARQUBE_URL?: string;
+}
+interface SonarqubeCredentials {
+  SONARQUBE_USER?: string;
+  SONARQUBE_PASSWORD?: string;
+}
+interface ArtifactoryConfig {
+  ARTIFACTORY_URL?: string;
+}
+interface ArtifactoryCredentials {
+  ARTIFACTORY_USER?: string;
+  ARTIFACTORY_PASSWORD?: string;
+}
+interface ArgoCdConfig {
+  ARGOCD_URL?: string;
+}
+interface ArgoCdCredentials {
+  ARGOCD_USER?: string;
+  ARGOCD_PASSWORD?: string;
+}
+
+type Configs = JenkinsConfig |
+  SonarqubeConfig |
+  ArtifactoryConfig |
+  ArgoCdConfig;
+
+type Creds = JenkinsCredentials |
+  SonarqubeCredentials |
+  ArtifactoryCredentials |
+  ArgoCdCredentials;
+
+type Info = Configs | Creds;
+
+type FlattenedInfo =
+  JenkinsConfig &
+  JenkinsCredentials &
+  SonarqubeConfig &
+  SonarqubeCredentials &
+  ArtifactoryConfig &
+  ArtifactoryCredentials &
+  ArgoCdConfig &
+  ArgoCdCredentials;
 
 const noopNotifyStatus: (status: string) => void = () => {};
 
 export async function getCredentials(namespace: string = 'tools', notifyStatus: (status: string) => void = noopNotifyStatus): Promise<Secrets> {
-  notifyStatus('Getting Jenkins credentials');
 
-  const jenkins = await getJenkinsInfo(namespace);
-
-  notifyStatus('Getting argocd credentials');
-
-  const argocd = await getArgoCdInfo(namespace);
-
-  notifyStatus('Getting sonarqube credentials');
-
-  const sonarqube = await getSonarqubeInfo(namespace);
-
-  return {
-    jenkins,
-    argocd,
-    sonarqube,
-  };
-}
-
-async function getJenkinsInfo(namespace: string = 'tools'): Promise<JenkinsAccessSecret | {}> {
-  try {
-    return await getSecretData('jenkins-access', namespace);
-  } catch (err) {
-    return {};
-  }
-}
-
-async function getSonarqubeInfo(namespace: string = 'tools'): Promise<ComponentInfo> {
-  return Object.assign(
-    {},
-    await getSonarqubeCredentials(namespace),
-    await getSonarqubeUrl(namespace),
+  const results: Info[] = await Promise.all(
+    configMaps(
+      [
+        'jenkins-config',
+        'sonarqube-config',
+        'artifactory-config',
+        'argocd-config'
+      ], namespace)
+      .concat(secrets(['sonarqube-access', 'artifactory-access'], namespace))
+      .concat(
+        getSecretData<ComponentCredentials>('jenkins-access', namespace)
+          .then<JenkinsCredentials>(c => {
+            return Object.assign(
+              {},
+              c.username ? {JENKINS_USER: c.username} : {},
+              c.password ? {JENKINS_PASSWORD: c.password} : {});
+          }),
+        getArgoCdCredentials(namespace),
+      )
   );
+
+  return group(flatten(results));
 }
 
-async function getSonarqubeCredentials(namespace: string = 'tools'): Promise<ComponentCredentials> {
-  try {
-    return await getSecretData('sonarqube-access', namespace);
-  } catch (err) {
-    return {};
-  }
+function configMaps(configMapNames: string[], namespace: string): Array<Promise<Info>> {
+  return configMapNames
+    .map(name => getConfigMapData(name, namespace).catch(err => ({})));
 }
 
-async function getSonarqubeUrl(namespace: string = 'tools'): Promise<ComponentUrl> {
-  try {
-    const host = (await getIngressHosts(namespace, 'sonarqube-sonarqube'))[0];
-
-    return {url: `http://${host}`};
-  } catch (err) {
-    return {};
-  }
+function secrets(secretNames: string[], namespace: string): Array<Promise<Info>> {
+  return secretNames
+    .map(name => getSecretData(name, namespace).catch(err => ({})));
 }
 
-async function getArgoCdInfo(namespace: string = 'tools'): Promise<ComponentInfo> {
-  return Object.assign(
-    {},
-    await getArgoCdCredentials(namespace),
-    await getArgoCdUrl(namespace),
-  );
-}
-
-async function getArgoCdCredentials(namespace: string = 'tools'): Promise<ComponentCredentials> {
+async function getArgoCdCredentials(namespace: string = 'tools'): Promise<ArgoCdCredentials> {
   const client = buildKubeClient();
 
   try {
     const result = await client.api.v1.namespace(namespace).pods.get();
 
-    const password = result.body.items
+    const ARGOCD_PASSWORD = result.body.items
       .map(item => item.metadata.name)
       .filter(name => name.startsWith('argocd-server'))[0];
 
-    return {username: 'admin', password};
+    return {ARGOCD_USER: 'admin', ARGOCD_PASSWORD};
   } catch (err) {
     return {};
   }
 }
 
-async function getArgoCdUrl(namespace: string = 'tools'): Promise<ComponentUrl> {
-  try {
-    const host = (await getIngressHosts(namespace, 'argocd-server-http'))[0];
-    return {url: `http://${host}`};
-  } catch (err) {
-    return {};
-  }
+function flatten(values: Info[]): FlattenedInfo {
+  return (values || []).reduce((result: FlattenedInfo, current: Info) => {
+    return Object.assign({}, result, current);
+  }, {});
+}
+
+function group(values: FlattenedInfo): Secrets {
+  return Object.keys(values)
+    .filter(key => key.split('_').length === 2)
+    .reduce((secrets: Secrets, compositeKey: string) => {
+      const group = compositeKey.toLowerCase().split('_')[0];
+      const key = compositeKey.toLowerCase().split('_')[1];
+
+      const groupValues = secrets[group] || {};
+      groupValues[key] = values[compositeKey];
+      secrets[group] = groupValues;
+
+      return secrets;
+    }, {});
 }
