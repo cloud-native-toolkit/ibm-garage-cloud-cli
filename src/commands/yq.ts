a82af2fb2bc830f46b48e94f5ee5172982fe7967
Sean Sundberg <seansund@us.ibm.com>
2020-03-26 07:30:59 -0500

    Updates documentation and removes unused commands (#20)
    - Removes `build-image`, `deploy-image`, `launch-tools`, and `login`
    - Updates the title reported in the help output
    - Adds link to Cloud Native Toolkit Dev Guide in help output
    - Adds a namespace flag to the `dashboard` command (ibm-garage-cloud/planning#197)
    - Adds `dashboard` command to README

--- src/commands/yq.ts
+++ src/commands/yq.ts
@@ -1,11 +1,7 @@
 import {Arguments, Argv} from 'yargs';
 
-import {buildImage, BuildOptions} from '../services/build-image';
-import {CommandLineOptions} from '../model';
-import {DefaultOptionBuilder} from '../util/yargs-support';
-
 export const command = 'yq <command>';
-export const desc = '';
+export const desc = 'lightweight yaml command-line processor that addresses deficiencies with the existing `yq` command';
 export const builder = (yargs: Argv<any>) => {
   return yargs.commandDir('yq_subcommands');
 };
