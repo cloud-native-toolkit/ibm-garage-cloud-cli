2f8a97097da32c1fdab3aab4f8d9a224110a67de
Sean Sundberg <seansund@us.ibm.com>
2020-03-30 16:01:30 -0500

    Removes logging statements

--- src/script-pipeline.ts
+++ src/script-pipeline.ts
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-console.log('argv before', process.argv);
-
 process.argv = process.argv.slice(0, 2).concat(['pipeline']).concat(process.argv.slice(2));
-
-console.log('argv after', process.argv);
 
 require('./script');
