a0a23dd015e3e3385e529b18629e76477f8f7bdb
Sean Sundberg <seansund@users.noreply.github.com>
2020-04-06 16:35:20 -0500

    git-secret: Updates logic for creating git secret and config map entries (#33)
    - Single git secret created in the namespace with name `git-credentials`
    - By default, if the secret already exists then don't change it. Only change if `replace` flag is set
    - Creates a config map to hold the details of the git repo - host, org, repo, url, and branch (for now)
    - Updates register pipeline to use new logic and skip prompt for credentials if the secret has already been created
    - Updates prompt messages for git secret creation
    
    ibm-garage-cloud/planning#127

--- src/services/git-secret/create-git-secret.ts
+++ src/services/git-secret/create-git-secret.ts
@@ -4,20 +4,31 @@ import {parse} from 'dot-properties';
 import {GitParams} from './git-params.model';
 import {GetGitParameters} from './git-parameters';
 import {GitParametersOptions} from './git-parameters-options.model';
-import {GitSecret} from './git-secret';
+import {GitSecret, SECRET_NAME} from './git-secret';
 import {FsPromises} from '../../util/file-util';
+import {KubeSecret} from '../../api/kubectl';
 
 export class CreateGitSecretOptions extends GitParametersOptions {
   namespaces: string[] | string;
   values?: string;
+  replace?: boolean;
 }
 
 export abstract class CreateGitSecret {
-  abstract async getParametersAndCreateSecret(options: CreateGitSecretOptions, notifyStatus?: (s: string) => void): Promise<GitParams>;
+  abstract async getParametersAndCreateSecret(options: CreateGitSecretOptions, notifyStatus?: (s: string) => void): Promise<{gitParams: GitParams, secretName: string, configMapName: string}>;
 
   abstract async getGitParameters(options: GitParametersOptions, notifyStatus?: (s: string) => void): Promise<GitParams>;
 
-  abstract async createGitSecret(gitParams: GitParams, namespaces: string | string[], valuesFile: string, notifyStatus?: (s: string) => void): Promise<string>;
+  abstract async createGitSecret(params: CreateGitSecretParams): Promise<{secretName: string, configMapName: string}>;
+}
+
+export interface CreateGitSecretParams {
+  gitParams: GitParams;
+  namespaces: string | string[];
+  valuesFile: string;
+  replace?: boolean;
+  name?: string;
+  notifyStatus?: (s: string) => void;
 }
 
 
@@ -28,37 +39,61 @@ const noopNotifyStatus = (s: string) => {
 @Provides(CreateGitSecret)
 export class CreateGitSecretImpl implements CreateGitSecret {
   @Inject
+  private kubeSecret: KubeSecret;
+  @Inject
   private gitParameters: GetGitParameters;
   @Inject
   private gitSecret: GitSecret;
   @Inject
   private fs: FsPromises;
 
-  async getParametersAndCreateSecret(options: CreateGitSecretOptions, notifyStatus: (s: string) => void = (s: string) => {return;}): Promise<GitParams> {
+  async getParametersAndCreateSecret(options: CreateGitSecretOptions, notifyStatus: (s: string) => void = (s: string) => {return;}): Promise<{gitParams: GitParams, secretName: string, configMapName: string}> {
 
-    const gitParams: GitParams = await this.getGitParameters(options, notifyStatus);
+    const updatedOptions: CreateGitSecretOptions = await this.updateOptionsFromCurrentSecret(options, notifyStatus);
 
-    await this.createGitSecret(
+    const gitParams: GitParams = await this.getGitParameters(updatedOptions, notifyStatus);
+
+    const {secretName, configMapName} = await this.createGitSecret({
       gitParams,
-      options.namespaces,
-      options.values,
-      notifyStatus
-    );
+      namespaces: updatedOptions.namespaces,
+      valuesFile: updatedOptions.values,
+      replace: updatedOptions.replace,
+      notifyStatus,
+    });
 
-    return gitParams;
+    return {gitParams, secretName, configMapName};
+  }
+
+  async updateOptionsFromCurrentSecret(options: CreateGitSecretOptions, notifyStatus: (s: string) => void): Promise<CreateGitSecretOptions> {
+    if (!options.replace && await this.kubeSecret.exists(SECRET_NAME, options.namespaces[0])) {
+      const {username, password} = await this.kubeSecret.getData(SECRET_NAME, options.namespaces[0]);
+
+      notifyStatus(`Git credentials have already been stored for user: ${username}`);
+
+      return Object.assign(
+        {},
+        options,
+        {
+          gitUsername: username,
+          gitPat: password,
+        })
+    } else {
+      return options;
+    }
   }
 
   async getGitParameters(options: GitParametersOptions, notifyStatus: (s: string) => void = (s: string) => {return;}): Promise<GitParams> {
     return this.gitParameters.getGitParameters(options, notifyStatus);
   }
 
-  async createGitSecret(gitParams: GitParams, namespaces: string | string[], valuesFile: string, notifyStatus: (s: string) => void = (s: string) => {return;}): Promise<string> {
-    return this.gitSecret.create(
+  async createGitSecret({gitParams, namespaces, valuesFile, name, replace, notifyStatus = (s: string) => {return;}}: CreateGitSecretParams): Promise<{secretName: string, configMapName: string}> {
+    return this.gitSecret.create({
       gitParams,
       namespaces,
-      await this.readValuesFile(valuesFile),
-      notifyStatus
-    );
+      additionalParams: await this.readValuesFile(valuesFile),
+      replace,
+      notifyStatus,
+    });
   }
 
   async readValuesFile(valuesFileName?: string): Promise<any> {
