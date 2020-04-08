f1e154d8e5a2573314a1584fa695d460ba46e07c
Sean Sundberg <seansund@us.ibm.com>
2020-04-07 19:48:57 -0500

    Updates createServiceAccount logic to fix issue of same secret added multiple times

---
+++ src/services/create-service-account/create-service-account.spec.ts
@@ -0,0 +1,43 @@
+import {CreateServiceAccountImpl} from './create-service-account';
+import {Container} from 'typescript-ioc';
+import {buildOptionWithEnvDefault} from '../../util/yargs-support';
+
+describe('create-service-account', () => {
+  test('canary verifies test infrastructure', () => {
+    expect(true).toEqual(true);
+  });
+
+  let classUnderTest: CreateServiceAccountImpl;
+  beforeEach(() => {
+    classUnderTest = Container.get(CreateServiceAccountImpl);
+  });
+
+  describe('given updateSecretList()', () => {
+    describe('when new secrets are provided', () => {
+      test('then the secret should be added', async () => {
+        const existingSecrets = [{name: 'secret1'}, {name: 'secret2'}];
+        const candidateSecret = ['newsecret'];
+
+        expect(
+          classUnderTest.updateSecretList(existingSecrets, candidateSecret)
+        ).toEqual(
+          {changed: true, updatedSecrets: existingSecrets.concat([{name: 'newsecret'}])}
+        );
+      });
+    });
+
+    describe('when an existing secret is provided', () => {
+      test('then don\'t add it', async () => {
+        const existingSecrets = [{name: 'secret1'}, {name: 'secret2'}];
+        const candidateSecret = ['secret2'];
+
+        expect(
+          classUnderTest.updateSecretList(existingSecrets, candidateSecret)
+        ).toEqual(
+          {changed: false, updatedSecrets: existingSecrets}
+        );
+      });
+    });
+  });
+
+});
