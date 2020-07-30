2b3410004a1ba9e9075f139dd2fa3b2a027683b9
Sean Sundberg <seansund@us.ibm.com>
2020-07-30 02:01:59 -0500

    git-repo: Adds command to launch git repo

--- src/services/git-secret/git-parameters-options.model.ts
+++ src/services/git-secret/git-parameters-options.model.ts
@@ -1,5 +1,6 @@
 
 export class GitParametersOptions {
+  remote?: string;
   workingDir?: string;
   gitUsername?: string;
   gitPat?: string;
--- src/services/git-secret/git-parameters.api.ts
+++ src/services/git-secret/git-parameters.api.ts
@@ -3,4 +3,5 @@ import {GitParams} from './git-params.model';
 
 export abstract class GetGitParameters {
   async abstract getGitParameters(options?: GitParametersOptions, notifyStatus?: (s: string) => void): Promise<GitParams>;
+  async abstract getGitConfig(remote?: string, workingDir?: string): Promise<{ url: string; host: string; org: string; repo: string }>;
 }
