0c5c5ec29f7186ff2052a175eef7fa24b68b91a5
Sean Sundberg <seansund@us.ibm.com>
2020-08-05 16:46:36 -0500

    Renames git-repo command to git

--- src/commands/git.ts
+++ src/commands/git.ts
@@ -5,9 +5,8 @@ import * as open from 'open';
 import {CommandLineOptions} from '../model';
 import {GetGitParameters} from '../services/git-secret';
 
-export const command = 'git-repo [remote]';
-export const aliases = ['git'];
-export const desc = 'Launches a browser to the git repo url specified by the origin remote';
+export const command = 'git [remote]';
+export const desc = 'Launches a browser to the git repo url specified by the remote. If not provided remote defaults to origin';
 export const builder = (argv: Argv<any>) => argv
   .positional('remote', {
     require: false,
