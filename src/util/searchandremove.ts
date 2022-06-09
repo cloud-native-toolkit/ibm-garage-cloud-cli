9283b306ec80ded0ad6518673e405109fa3fb0ff
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-09 21:12:39 +0530

    Added function comment
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- src/util/searchandremove.ts
+++ src/util/searchandremove.ts
@@ -1,12 +1,14 @@
 import {KubeMetadata} from "src/api/kubectl";
 
+//This function searches for app.kubernetes.io/instance and argocd.argoproj.io/instance labels by default in metadata and removes them. It also has an optional deletelabel input.
+//When value is passed to deletelabel, it also deletes deletelabel label from metadata other than app.kubernetes.io/instance and argocd.argoproj.io/instance labels. 
 export function searchAndRemove<T extends KubeMetadata>(text: T ,deletelabel?: string): T {
       console.log("Test",text);
       var labelkey01 = "app.kubernetes.io/instance";
       var labelkey02 = "argocd.argoproj.io/instance";
       
           var json: any = text.labels;
-          console.log("Am I reachable",json);
+          //console.log("Am I reachable",json);
           for (var key in json) {
               if (json.hasOwnProperty(labelkey01)) {
               
