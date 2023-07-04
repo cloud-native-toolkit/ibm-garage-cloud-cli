bb9c3465fe9ea7e463d492478d0dd661a530fb33
Sean Sundberg <seansund@us.ibm.com>
2023-07-04 10:23:51 -0500

    Fix ClusterVersion api resource
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/api/kubectl/cluster-version.ts
+++ src/api/kubectl/cluster-version.ts
@@ -1,7 +1,12 @@
 import {BuildContext, Factory, ObjectFactory} from 'typescript-ioc';
 
 import {AsyncKubeClient} from './client';
-import {AbstractKubernetesNamespacedResource, KubeResource, Props} from './kubernetes-resource-manager';
+import {
+  AbstractKubernetesClusterResource,
+  CustomResourceDefinition,
+  KubeResource,
+  Props
+} from './kubernetes-resource-manager';
 
 export interface ClusterVersionDesired {
   image: string
@@ -19,18 +24,130 @@ export interface ClusterVersion extends KubeResource {
 }
 
 const factory: ObjectFactory = (context: BuildContext) => {
+  const group = 'config.openshift.io'
+  const version = 'v1'
+  const kind = 'ClusterVersion'
+  const name = 'clusterversions'
+  const singular = 'clusterversion'
+  const crd: CustomResourceDefinition = {
+    apiVersion: `${group}/${version}`,
+    kind,
+    metadata: {
+      name: `${name}.${group}`,
+      annotations: {},
+    },
+    spec: {
+      conversion: {
+        strategy: 'None'
+      },
+      group,
+      names: {
+        categories: ['openshift'],
+        kind,
+        listKind: `${kind}List`,
+        plural: name,
+        shortNames: [],
+        singular,
+      },
+      scope: 'Cluster',
+      versions: [{
+        additionalPrinterColumns: [],
+        name: version,
+        schema: {
+          openAPIV3Schema: {
+            description: 'Subscription keeps operators up to date by tracking changes to Catalogs.',
+            properties: {
+              apiVersion: {
+                description: 'APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources',
+                type: 'string',
+              },
+              kind: {
+                description: 'Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds',
+                type: 'string',
+              },
+              metadata: {
+                type: 'object',
+              },
+              spec: {
+                type: 'object',
+                properties: {
+                  channel: {
+                    type: 'string'
+                  },
+                  clusterID: {
+                    type: 'string'
+                  },
+                  desiredUpdate: {
+                    type: 'object'
+                  },
+                  overrides: {
+                    type: 'array'
+                  },
+                  upstream: {
+                    type: 'string'
+                  }
+                }
+              },
+              status: {
+                type: 'object',
+                properties: {
+                  availableUpdates: {
+                    type: 'object'
+                  },
+                  conditionalUpdates: {
+                    type: 'array'
+                  },
+                  conditions: {
+                    type: 'array'
+                  },
+                  desired: {
+                    type: 'object',
+                    properties: {
+                      channels: {
+                        type: 'array',
+                        items: {
+                          type: 'string'
+                        }
+                      },
+                      image: {
+                        type: 'string'
+                      },
+                      url: {
+                        type: 'string'
+                      },
+                      version: {
+                        type: 'string'
+                      }
+                    }
+                  },
+                  versionHash: {
+                    type: 'string'
+                  }
+                }
+              },
+            },
+          },
+        },
+      }],
+      served: true,
+      storage: true,
+      subresources: {
+        status: {}
+      }
+    },
+  }
   return new KubeClusterVersion({
     client: context.resolve(AsyncKubeClient),
-    group: 'config.openshift.io',
-    version: 'v1',
-    name: 'clusterversions',
-    kind: 'ClusterVersion',
-    crd: true,
+    group,
+    version,
+    name,
+    kind,
+    crd,
   });
 };
 
 @Factory(factory)
-export class KubeClusterVersion extends AbstractKubernetesNamespacedResource<ClusterVersion> {
+export class KubeClusterVersion extends AbstractKubernetesClusterResource<ClusterVersion> {
   constructor(props: Props) {
     super(props);
   }
